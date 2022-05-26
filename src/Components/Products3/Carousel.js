6ede2b129fd55abceb14e146cf0bf56a0415ac24
Gaston Vignau <vignaugaston@gmail.com>
2022-05-26 01:00:22 -0300

    third commit

--- src/Components/Products3/Carousel.js
+++ src/Components/Products3/Carousel.js
@@ -8,28 +8,30 @@ const Carousel = () => {
         <div className='divImage'>
             <img className='imageCarousel' src={section3image} alt="..."/>
         </div>
-        <div className='textCarousel'>
-            <div id="carouselExampleControls" className="carousel slide slideArrow" data-bs-ride="carousel">
-                <div className='carousel-inner'>
-                <div className="carousel-item active">
-                    <h4 className='h4Carousel'>Sed ut perspiciatis</h4>
-                    <h3 className='h3Carousel'>Nemo Enim</h3>
-                    <p className='parraCarousel'>Sed ut perspiciatis unde imnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+        <div className='containerCarousel'>
+            <div className='textCarousel'>
+                <div id="carouselExampleControls" className="carousel slide slideArrow" data-bs-ride="carousel">
+                    <div className='carousel-inner'>
+                    <div className="carousel-item active">
+                        <h4 className='h4Carousel'>Sed ut perspiciatis</h4>
+                        <h3 className='h3Carousel'>Nemo Enim</h3>
+                        <p className='parraCarousel'>Sed ut perspiciatis unde imnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                    </div>
+                    <div className="carousel-item">
+                        <h4 className='h4Carousel'>Egestas diam in</h4>
+                        <h3 className='h3Carousel'>Molestie ac</h3>
+                        <p className='parraCarousel'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    </div>
+                    <button className='carousel-control-prev' type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span className='carousel-control-prev-icon' aria-hidden="true"></span>
+                        <span className='visually-hidden'>Previous</span>
+                    </button>
+                    <button className='carousel-control-next' type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
                 </div>
-                <div className="carousel-item">
-                    <h4 className='h4Carousel'>Egestas diam in</h4>
-                    <h3 className='h3Carousel'>Molestie ac</h3>
-                    <p className='parraCarousel'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
-                </div>
-                <button className='carousel-control-prev' type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span className='carousel-control-prev-icon' aria-hidden="true"></span>
-                    <span className='visually-hidden'>Previous</span>
-                </button>
-                <button className='carousel-control-next' type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
             </div>
         </div>
     </div>
